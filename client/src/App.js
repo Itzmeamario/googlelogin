c54bd4d4e16cda187eb17e13fb122d16e3870e9b
itzmeamario <mario.santamaria.portilla@gmail.com>
2018-08-31 20:40:49 -0700

    changed version of babel-core, was presenting issues with version

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+// export default class App extends React.Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+        Hi
+      </div>
+    );
+  }
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const parser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+const helmet = require('helmet');
+
 const router = require('./router');
 
 const app = express();
@@ -8,12 +12,15 @@ const app = express();
 app.set('port', 3000);
 
 app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
 
 app.use('/api/post', parser.json());
 app.use('/api/post', parser.urlencoded({ extended: false }))
 
 app.use('/api', router);
 
+app.use(express.static(path.join(__dirname, '../client/dist')))
 
 app.listen(app.get('port'), (error) => {
   if(error) {
